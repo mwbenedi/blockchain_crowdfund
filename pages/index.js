ed675517c60a044c4f30d5159f11c266557cec2a
mwbenedi <mwbenedi@outlook.com>
2019-05-06 15:02:48 -0600

    more coursework

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -14,7 +15,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                    <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             };
         });
@@ -27,8 +32,13 @@ class CampaignIndex extends Component {
         <Layout>
             <div>
                 <h3>Open Campaigns</h3>
-            <Button content="Create Campaign" icon="add circle" primary floated="right"/>
-            {this.renderCampaigns()}
+
+                <Link route="/campaigns/new">
+                    <a>
+                        <Button content="Create Campaign" icon="add circle" primary floated="right"/>
+                    </a>
+                </Link>
+                {this.renderCampaigns()}
             </div>
         </Layout>)
     }
